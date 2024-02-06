833e0c9e551014d988f4585403fc1b7cafa9d62c
itc-23032 <n23032@std.it-college.ac.jp>
2024-02-06 05:34:08 +0000

    7.6 データ取得

--- components/meta.js
+++ components/meta.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
-import { useRouter } from 'next/router'
-
 import { siteMeta } from 'lib/constants'
+import { useRouter } from 'next/router'
+import siteImg from 'images/ogp.jpg' // 汎用OGP画像
 const {
   siteTitle,
   siteDesc,
@@ -11,30 +11,29 @@ const {
   siteIcon
 } = siteMeta
 
-import siteImg from 'images/ogp.jpg'
-
-const Meta = ({ pageTitle, pageDesc, pageImg, pageImgW, pageImgH }) => {
-  const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle
-
-  const desc = pageDesc ?? siteDesc
+const Meta = props => {
+  const title = props.pageTitle
+    ? `${props.pageTitle} | ${siteTitle}`
+    : siteTitle
+  const desc = props.pageDesc ?? siteDesc
 
   const router = useRouter()
   const url = `${siteUrl}${router.asPath}`
 
-  const img = pageImg || siteImg.src
-  const imgW = pageImgW || siteImg.width
-  const imgH = pageImgH || siteImg.height
+  // OGP画像
+  const img = props.Img || siteImg.src
+  const imgW = props.pageImgW || siteImg.width
+  const imgH = props.pageImgH || siteImg.height
   const imgUrl = img.startsWith('https') ? img : `${siteUrl}${img}`
-
   return (
     <Head>
       <title>{title}</title>
       <meta property='og:title' content={title} />
 
       <meta name='description' content={desc} />
-      <meta property='ag:description' content={desc} />
+      <meta property='og:description' content={desc} />
 
-      <link rel='cannonical' href={url} />
+      <link rel='canonical' href={url} />
       <meta property='og:url' content={url} />
 
       <meta property='og:site_name' content={siteTitle} />
@@ -47,9 +46,8 @@ const Meta = ({ pageTitle, pageDesc, pageImg, pageImgW, pageImgH }) => {
       <meta property='og:image' content={imgUrl} />
       <meta property='og:image:width' content={imgW} />
       <meta property='og:image:height' content={imgH} />
-      <meta priperty='twitter:card' content='summary_large_image' />
+      <meta name='twitter:card' content='summary_large_image' />
     </Head>
   )
 }
-
 export default Meta
